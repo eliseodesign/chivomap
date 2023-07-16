904e9a4179b6bb5a4409ea80e0bf8d5a8446b80a
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-16 09:57:28 -0600

    feat: :sparkles: install react-icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: "#172554",
+        secondary: "#93c5fd"
+      }
+    },
   },
   plugins: [],
 }
