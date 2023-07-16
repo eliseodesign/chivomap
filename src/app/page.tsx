1848124758534c965d3b8ebbbe04e9acd5d7874c
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-16 07:10:18 -0600

    feat: :sparkles: install and config tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <h1>ChivoMap</h1>
+        <h1 className="text-3xl font-bold underline">ChivoMap</h1>
         <p>Project in progress</p>
       </div>
     </main>
